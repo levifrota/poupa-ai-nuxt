675d85a11824b65c7e112ee0970e83400463b33a
Gideao Frota <gideao.frota@blip.ai>
2025-04-13 14:36:48 -0300

    feat: initialize project

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Button } from '@/app/_components/ui/button'
+
+</script>
+
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <Button>Button</Button> 
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/app/assets/css/tailwind.css'],
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    'shadcn-nuxt',
+    '@vite-pwa/nuxt',
+  ],
+
+  vite: {
+    plugins: [tailwindcss(),]
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./app/_components/ui"
+     */
+    componentDir: './app/_components/ui'
+  },
+
+  pwa: {
+    
+  }
+})
