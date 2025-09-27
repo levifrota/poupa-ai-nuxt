d539fc921a8d410434f537ae23568f5f68571f60
Gideao Frota <gideao.frota@blip.ai>
2025-09-27 17:55:01 -0300

    feat: conditionally render Navbar and Footer based on authentication page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <NavbarComponent />
-    <main class="flex-grow p-4">
-      <NuxtPage />
-    </main>
-    <FooterComponent />
+    <template v-if="!isAuthPage">
+      <NavbarComponent />
+      <main class="flex-grow p-4">
+        <NuxtPage />
+      </main>
+      <FooterComponent />
+    </template>
+    <template v-else>
+      <main>
+        <NuxtPage />
+      </main>
+    </template>
   </div>
 </template>
 
 <script setup>
 import NavbarComponent from "@/components/NavbarComponent.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const isAuthPage = computed(() => {
+  return route.path === "/login" || route.path === "/register";
+});
 </script>
 
 <style></style>
